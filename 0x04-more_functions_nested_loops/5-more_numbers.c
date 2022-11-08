6029ee30f413a2bc0c7a94466e625afda8fa7ee8
Oluwatooniloba <erinleoluwatoniloba@gmail.com>
2022-11-08 14:17:57 +0000

    Numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,8 +15,8 @@ void more_numbers(void)
 		for (b = 0; b <= 14; b++)
 	{
 			if (b >= 10)
-			_putchar(49);
-			_putchar(b % 10 + 48);
+			_putchar('1');
+			_putchar(b % 10 + '0');
 	}
 	_putchar('\n');
 	}
